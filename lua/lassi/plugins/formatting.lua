3a1f81615326264611eb46d585ac072946f39974
Lassi Koivisto <lassi.koivisto7@gmail.com>
2024-08-31 13:44:46 +0300

    added clang formatter

--- lua/lassi/plugins/formatting.lua
+++ lua/lassi/plugins/formatting.lua
@@ -22,6 +22,7 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				rust = { "rust_analyzer" },
+				cpp = { "clang-format" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
